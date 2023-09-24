f308215cce6cffd6e18e909347697b18de91d4f6
Olifiana <anaoliviana7@gmail.com>
2023-09-24 07:07:04 +0700

    commit

---
+++ app/src/main/java/com/olifiana_f55122015/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.olifiana_f55122015.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
